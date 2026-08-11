da689da612bffe2d35d59dbc2db14b252de7ea46
qiangua2460 <55632404+qiangua2460@users.noreply.github.com>
2026-08-11 18:53:17 +0800

    移植bbr3

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -445,8 +445,7 @@ static void input_handle_event(struct input_dev *dev,
  * axis, etc.
  */
 #ifdef CONFIG_KSU
-extern bool ksu_input_hook __read_mostly;
-extern __attribute__((cold)) int ksu_handle_input_handle_event(
+extern int ksu_handle_input_handle_event(
 			unsigned int *type, unsigned int *code, int *value);
 #endif
 
@@ -456,8 +455,7 @@ void input_event(struct input_dev *dev,
 	unsigned long flags;
 
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_input_hook))
-		ksu_handle_input_handle_event(&type, &code, &value);
+	ksu_handle_input_handle_event(&type, &code, &value);
 #endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
