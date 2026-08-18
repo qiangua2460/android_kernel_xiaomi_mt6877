170f22b9f67548f6d832ce12a08dd1a9f7b299ab
qiangua2460 <55632404+qiangua2460@users.noreply.github.com>
2026-08-18 23:01:39 +0800

    优化数值

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7649,7 +7649,7 @@ int __meminit init_per_zone_wmark_min(void)
 	int new_min_free_kbytes;
 
 	lowmem_kbytes = nr_free_buffer_pages() * (PAGE_SIZE >> 10);
-	new_min_free_kbytes = int_sqrt(lowmem_kbytes * 16);
+	new_min_free_kbytes = int_sqrt(lowmem_kbytes * 16) / 2;
 
 	if (new_min_free_kbytes > user_min_free_kbytes) {
 		min_free_kbytes = new_min_free_kbytes;
