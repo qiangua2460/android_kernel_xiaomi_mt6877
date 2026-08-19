f9541068de7e265049eb7eaece011ac49b7f5ddf
qiangua2460 <55632404+qiangua2460@users.noreply.github.com>
2026-08-19 16:13:52 +0800

    更新 sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1269,6 +1269,14 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
+		strcpy(tmp.release, "5.10.260");
+		pr_debug("fake uname: %s release=%s\n",
+			 current->comm, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
